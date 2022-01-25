ee103a87f42f24b969085cbcc2e0d70408abea3d
Louis <karpx033@umn.edu>
2022-01-25 17:55:35 -0600

    debugged my logic on how my modularized files were linked. Note html now loads with databased data

--- routes/notes.js
+++ routes/notes.js
@@ -3,23 +3,24 @@ const fb = require('express').Router();
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the feedback
-fb.get('/notes', (req, res) =>
+fb.get('/', (req, res) =>
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
 // POST Route for submitting feedback
-fb.post('/notes', (req, res) => {
+fb.post('/', (req, res) => {
   // Destructuring assignment for the items in req.body
   const { noteTitle, noteText } = req.body;
 
   // If all the required properties are present
   if (noteTitle && noteText) {
+      
     // Variable for the object we will save
     const newEntry = {
       noteTitle,
       noteText,
     };
-
+    console.log(newEntry);
     readAndAppend(newEntry, './db/db.json');
 
     const response = {
