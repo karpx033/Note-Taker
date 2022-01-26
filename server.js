a4c85f0a4c5523aad66bd8fbc4583f8357e1dd16
Louis <karpx033@umn.edu>
2022-01-25 21:26:50 -0600

    troubleshooting heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT;
 
 const app = express();
 
